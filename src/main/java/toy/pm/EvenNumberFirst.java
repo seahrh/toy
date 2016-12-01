cccde2e8a13260d29c339ff51ee3254c3045e6c4
Ruhong <seahrh@gmail.com>
2016-12-01 09:03:23 +0800

    Add comments on even-odd number partitioning

--- src/main/java/toy/pm/EvenNumberFirst.java
+++ src/main/java/toy/pm/EvenNumberFirst.java
@@ -9,7 +9,25 @@ public final class EvenNumberFirst {
 	private EvenNumberFirst() {
 		// Not meant to be instantiated
 	}
-	
+
+	/**
+	 * Shuffle an integer array into two partitions: the first partition
+	 * contains all even numbers and the second partition contains all odd
+	 * numbers.
+	 * <p>
+	 * Have two pointers start at the head and tail of the array, and walk the
+	 * two pointers towards the middle of the array. Increment the head pointer
+	 * until an odd number is found. Decrement the tail pointer until an even
+	 * number is found. Swap the elements in the two pointers. Stop the loop
+	 * when the two pointers meet. Based on quicksort's Hoare partitioning
+	 * scheme.
+	 * <p>
+	 * This takes O(n) time as n comparisons are made. The shuffling occurs
+	 * in-place by swapping iteratively (no recursion), therefore space is O(1).
+	 * 
+	 * @param arr
+	 *            integer array - must not be null or empty
+	 */
 	public static void partition(int[] arr) {
 		if (arr == null) {
 			log.error("Array must not be null");
@@ -40,7 +58,17 @@ public final class EvenNumberFirst {
 			j--;
 		}
 	}
-	
+
+	/**
+	 * Swap the array elements at the specified indices.
+	 * 
+	 * @param arr
+	 *            integer array - must not be null or empty
+	 * @param thisIndex
+	 *            index of the array element to be swapped
+	 * @param thatIndex
+	 *            index of the array element to be swapped
+	 */
 	private static void swap(int[] arr, int thisIndex, int thatIndex) {
 		if (arr == null) {
 			log.error("Array must not be null");
