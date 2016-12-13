5a2caa13b39d03e3ba047e8e5de01bf0e1f758cd
Ruhong <seahrh@gmail.com>
2016-12-13 15:50:59 +0800

    Specify version of maven-compiler-plugin, fix compile error

--- src/main/java/toy/_default/StringPermutation.java
+++ src/main/java/toy/_default/StringPermutation.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import com.google.common.collect.Sets;
 
 public class StringPermutation {
-	private static final Logger log = LoggerFactory.getLogger(StringPermutationTest.class);
+	private static final Logger log = LoggerFactory.getLogger(StringPermutation.class);
 
 	private StringPermutation() {
 		// Not meant to be instantiated
