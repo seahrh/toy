fa17b60b32bce1373d94acd3ad712eaf6bf49430
Ruhong <seahrh@gmail.com>
2017-01-12 15:48:52 +0800

    Create package for recommender on Book Crossing dataset

---
+++ src/main/java/toy/bx/ItemSimilarity.java
@@ -0,0 +1,19 @@
+package toy.bx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ItemSimilarity {
+	private static final Logger log = LoggerFactory.getLogger(ItemSimilarity.class);
+	private static final String INPUT_FILE_PATH = System.getProperty("toy.in");
+	private static final String OUTPUT_FILE_PATH = System.getProperty("toy.out");
+
+/*	public static void main(String[] args) throws IOException {
+		String[][] sheet = input();
+		
+	}
+	
+	private static List<List<String>> input() {
+		
+	}*/
+}
