0bde5dbef5b89abbcb0418fd702813ab578ce44b
Ruhong <seahrh@gmail.com>
2017-01-14 19:46:34 +0800

    Tweak binary search to return nearest value if key is not found

--- src/main/java/toy/_default/BinarySearch.java
+++ src/main/java/toy/_default/BinarySearch.java
@@ -1,19 +1,77 @@
 package toy._default;
 
-public class BinarySearch {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class BinarySearch {
+	private static final Logger log = LoggerFactory.getLogger(BinarySearch.class);
 
 	private BinarySearch() {
 		// Not meant to be instantiated
 	}
-
-	public static int binarySearch(int[] arr, int begin, int end, int key) {
-		if (arr == null || arr.length == 0) {
+	
+	/**
+	 * Binary search to get index of key.
+	 * If key is not found, return index of nearest value.
+	 * 
+	 * @param arr
+	 * @param begin
+	 * @param end
+	 * @param key
+	 * @param nearest
+	 * @return
+	 */
+	private static int nearest(int[] arr, int begin, int end, int key, int nearest) {
+		int len = arr.length;
+		if (begin < 0) {
+			log.error("begin index must not be less than zero");
 			throw new IllegalArgumentException();
 		}
+		if (end >= len) {
+			log.error("end index must not be greater than last index [{}]", len - 1);
+			throw new IllegalArgumentException();
+		}
+		if (nearest < 0 || nearest >= len) {
+			log.error("index of nearest value out of bounds [{}]", nearest);
+			throw new IllegalArgumentException();
+		}
+		// 1st base case: key not found
+		if (begin > end) {
+			return nearest;
+		}
+		// Avoid integer overflow
+		int mid = (end - begin) / 2 + begin;
+		int vmid = arr[mid];
+		// 2nd base case: key found
+		if (vmid == key) {
+			return mid;
+		}
+		int vnearest = arr[nearest];
+		if (Math.abs(key - vmid) < Math.abs(key - vnearest)) {
+			nearest = mid;
+		}
+		if (key < vmid) {	
+			return nearest(arr, begin, mid - 1, key, nearest);
+		}
+		return nearest(arr, mid + 1, end, key, nearest);
+	}
+	
+	public static int nearest(int[] arr, int key) {
+		if (arr == null || arr.length == 0) {
+			log.error("array must not be null or empty");
+			throw new IllegalArgumentException();
+		}
+		// Nearest value initialized as the 1st element
+		return nearest(arr, 0, arr.length - 1, key, 0);
+	}
+
+	private static int search(int[] arr, int begin, int end, int key) {
 		if (begin < 0) {
+			log.error("begin index must not be less than zero");
 			throw new IllegalArgumentException();
 		}
 		if (end >= arr.length) {
+			log.error("end index must not be greater than last index [{}]", arr.length - 1);
 			throw new IllegalArgumentException();
 		}
 		// 1st base case: key not found
@@ -22,22 +80,23 @@ public class BinarySearch {
 		}
 		// Avoid integer overflow
 		int mid = (end - begin) / 2 + begin;
-		int midv = arr[mid];
+		int vmid = arr[mid];
 		// 2nd base case: key found
-		if (midv == key) {
+		if (vmid == key) {
 			return mid;
 		}
-		if (key < midv) {
-			return binarySearch(arr, begin, mid - 1, key);
+		if (key < vmid) {
+			return search(arr, begin, mid - 1, key);
 		}
-		return binarySearch(arr, mid + 1, end, key);
+		return search(arr, mid + 1, end, key);
 	}
 
-	public static int binarySearch(int[] arr, int key) {
+	public static int search(int[] arr, int key) {
 		if (arr == null || arr.length == 0) {
+			log.error("array must not be null or empty");
 			throw new IllegalArgumentException();
 		}
-		return binarySearch(arr, 0, arr.length - 1, key);
+		return search(arr, 0, arr.length - 1, key);
 	}
 
 }
--- src/test/java/toy/_default/BinarySearchTest.java
+++ src/test/java/toy/_default/BinarySearchTest.java
@@ -6,33 +6,52 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class BinarySearchTest {
-	@Test(dataProvider = "data")
-	public void test(int[] in, int key, int expected, String description) {
-		int actual = BinarySearch.binarySearch(in, key);
+	@Test(dataProvider = "nearestData")
+	public void nearestTest(int[] in, int key, int expected, String description) {
+		int actual = BinarySearch.nearest(in, key);
 		assertEquals(actual, expected);
 	}
 
 	@DataProvider
-	public Object[][] data() {
+	public Object[][] nearestData() {
+		final int[] evenSorted = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+		final int[] oddSorted = new int[] { 1, 2, 3, 4, 5, 6, 7, 17, 27 };
+		return new Object[][] {
+				{ new int[] { 1 }, 1, 0, "len 1 array, key exists" },
+				{ new int[] { 1 }, 2, 0, "len 1 array, nearest value is 1" },
+				{ new int[] { 1, 2 }, 2, 1, "len 2 array, key exists" },
+				{ new int[] { 1, 2 }, 3, 1, "len 2 array, nearest value is 2" },
+				{ evenSorted, 3, 2, "len 8 array, key exists" },
+				{ evenSorted, 0, 0, "len 8 array, nearest value is 1" },
+				{ evenSorted, 10, 7, "len 8 array, nearest value is 7" },
+				{ oddSorted, 7, 6, "len 9 array, key exists" },
+				{ oddSorted, 10, 6, "len 9 array, nearest value is 7" },
+				{ oddSorted, 12, 6,
+						"len 9 array, nearest value is 7 or 17 (both equidistant)" },
+				{ oddSorted, 20, 7, "len 9 array, nearest value is 17" } };
+	}
+
+	@Test(dataProvider = "searchData")
+	public void searchTest(int[] in, int key, int expected, String description) {
+		int actual = BinarySearch.search(in, key);
+		assertEquals(actual, expected);
+	}
+
+	@DataProvider
+	public Object[][] searchData() {
 		final int[] evenSorted = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 		final int[] oddSorted = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		return new Object[][] {
 				{ new int[] { 1 }, 1, 0, "len 1 array, key exists" },
-				{ new int[] { 1 }, 2, -1,
-						"len 1 array, key does not exist" },
+				{ new int[] { 1 }, 2, -1, "len 1 array, key does not exist" },
 				{ new int[] { 1, 2 }, 2, 1, "len 2 array, key exists" },
-				{ new int[] { 1, 2 }, 3, -1,
-						"len 2 array, key does not exist" },
+				{ new int[] { 1, 2 }, 3, -1, "len 2 array, key does not exist" },
 				{ evenSorted, 3, 2, "len 8 array, key exists" },
-				{ evenSorted, 0, -1,
-						"len 8 array, key does not exist" },
-				{ evenSorted, 10, -1,
-						"len 8 array, key does not exist" },
+				{ evenSorted, 0, -1, "len 8 array, key does not exist" },
+				{ evenSorted, 10, -1, "len 8 array, key does not exist" },
 				{ oddSorted, 7, 6, "len 9 array, key exists" },
-				{ oddSorted, 0, -1,
-						"len 9 array, key does not exist" },
-				{ oddSorted, 10, -1,
-						"len 9 array, key does not exist" } };
+				{ oddSorted, 0, -1, "len 9 array, key does not exist" },
+				{ oddSorted, 10, -1, "len 9 array, key does not exist" } };
 	}
 
 }
