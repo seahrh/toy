c4f78781d735a601cfd016bd50d546481f404fb0
Ruhong <seahrh@gmail.com>
2017-01-21 12:48:47 +0800

    Evaluate test set, print results

--- src/main/java/toy/bx/ItemCfTester.java
+++ src/main/java/toy/bx/ItemCfTester.java
@@ -1,8 +1,9 @@
 package toy.bx;
 
+import static toy.bx.ItemCf.predict;
+
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,11 +12,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import toy.util.FileUtil;
+import toy.util.MathUtil;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableTable;
+import com.google.common.primitives.Doubles;
 
 public final class ItemCfTester {
 	private static final Logger log = LoggerFactory.getLogger(ItemCfTester.class);
@@ -34,8 +36,47 @@ public final class ItemCfTester {
 		long startTime = System.currentTimeMillis();
 		log.info("main: task started...");
 		init();
+		eval();
 		long elapsedTime = System.currentTimeMillis() - startTime;
 		log.info("main: task completed ({}s)", elapsedTime / 1000);
+	}
+
+	private static void eval() {
+		long startTime = System.currentTimeMillis();
+		log.info("init: task started...");
+		List<Double> predictions = new ArrayList<>(testSet.size());
+		List<Double> actuals = new ArrayList<>(testSet.size());
+		String isbn;
+		String uid;
+		Double a;
+		Optional<Double> op;
+		int skipped = 0;
+		for (List<String> tokens : testSet) {
+			uid = tokens.get(0)
+				.toLowerCase();
+			isbn = tokens.get(1)
+				.toLowerCase();
+			a = Double.parseDouble(tokens.get(2));
+			op = predict(uid, isbn, ratingTable, simMatrix);
+			if (!op.isPresent()) {
+				skipped++;
+				continue;
+			}
+			actuals.add(a);
+			predictions.add(op.get());
+		}
+		double[] predictionsArr = Doubles.toArray(predictions);
+		double[] actualsArr = Doubles.toArray(actuals);
+		double meanAbsoluteError = MathUtil.meanAbsoluteError(predictionsArr,
+				actualsArr);
+		double rootMeanSquaredError = MathUtil.rootMeanSquaredError(
+				predictionsArr, actualsArr);
+		log.info(
+				"Results:\nmeanAbsoluteError={}\nrootMeanSquaredError={}\n#predictions={}\n#skipped",
+				meanAbsoluteError, rootMeanSquaredError, predictionsArr.length,
+				skipped);
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		log.info("init: task completed ({}s)", elapsedTime / 1000);
 	}
 
 	private static void init() throws IOException {
@@ -52,7 +93,8 @@ public final class ItemCfTester {
 		long startTime = System.currentTimeMillis();
 		log.info("importSimilarityMatrix: started...");
 		final CharMatcher separator = CharMatcher.anyOf("\t");
-		List<List<String>> in = FileUtil.read(SIMILARITY_MATRIX_FILE_PATH, separator);
+		List<List<String>> in = FileUtil.read(SIMILARITY_MATRIX_FILE_PATH,
+				separator);
 		String pair;
 		Double sim;
 		for (List<String> tokens : in) {
