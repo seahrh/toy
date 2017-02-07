e11c635f40d5dffc3f259c7cdfc535143796c2c7
Ruhong <seahrh@gmail.com>
2017-02-07 13:01:46 +0800

    Find subsequence with the largest sum in an array of positive and negative integers

--- src/main/java/toy/_default/SequenceSum.java
+++ src/main/java/toy/_default/SequenceSum.java
@@ -3,19 +3,6 @@ package toy._default;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
- * Facebook interview question round 1 (Nov 2016)
- * --------------------------------------------------
- Question:  Given a sequence of positive integers A and an integer T, return whether there is a *continuous sequence* of A that sums up to exactly T
- Example
- [23, 5, 4, 7, 2, 11], 20. Return True because 7 + 2 + 11 = 20
- [1, 3, 5, 23, 2], 8. Return True  because 3 + 5 = 8
- [1, 3, 5, 23, 2], 7 Return False because no sequence in this array adds up to 7
-
- Note: We are looking for an O(N) solution. There is an obvious O(N^2) solution which is a good starting point but is not the final solution we are looking for.
- ----------------------------------------------------
- */
-
 public final class SequenceSum {
 	private static final Logger log = LoggerFactory.getLogger(SequenceSum.class);
 
@@ -24,7 +11,62 @@ public final class SequenceSum {
 	}
 
 	/**
-	 * O(n) solution based on @url https://www.careercup.com/question?id=6305076727513088
+	 * You are given an array of integers (both positive and negative). Find the
+	 * continuous sequence with the largest sum. Return the sum. [CTCI Q19.7]
+	 * <p>
+	 * EXAMPLE Input: {2, -8, 3, -2, 4, -10}
+	 * <p>
+	 * Output: 5 (i.e., {3, -2, 4} )
+	 * <p>
+	 * Assume subsequence cannot be empty if the array contains all negative
+	 * integers.
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public static int max(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			log.error("array must not be null or empty");
+			throw new IllegalArgumentException();
+		}
+		// Initialize max at lowest possible to handle negative numbers
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			if (sum > max) {
+				max = sum;
+			} else if (sum < 0) {
+				// If sum drops below zero, that subsequence will not
+				// contribute to the maximal subsequence since max is reduced by
+				// adding the negative sum.
+				// If the array contains all negative numbers, sum is reset for
+				// each element, effectively picking the smallest negative
+				// number.
+				sum = 0;
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * Facebook interview question round 1 (Nov 2016)
+	 * --------------------------------------------------
+	 * <p>
+	 * Question: Given a sequence of positive integers A and an integer T,
+	 * return whether there is a *continuous sequence* of A that sums up to
+	 * exactly T Example [23, 5, 4, 7, 2, 11], 20. Return True because 7 + 2 +
+	 * 11 = 20 [1, 3, 5, 23, 2], 8. Return True because 3 + 5 = 8 [1, 3, 5, 23,
+	 * 2], 7 Return False because no sequence in this array adds up to 7
+	 * 
+	 * Note: We are looking for an O(N) solution. There is an obvious O(N^2)
+	 * solution which is a good starting point but is not the final solution we
+	 * are looking for.
+	 * <p>
+	 * ----------------------------------------------------
+	 * <p>
+	 * O(n) solution based on @url
+	 * https://www.careercup.com/question?id=6305076727513088
 	 * 
 	 * @param arr
 	 * @param target
--- src/test/java/toy/_default/SequenceSumTest.java
+++ src/test/java/toy/_default/SequenceSumTest.java
@@ -7,19 +7,42 @@ import org.testng.annotations.Test;
 
 public class SequenceSumTest {
 
-	@Test(dataProvider = "data")
-	public void test(int[] a, int t, boolean expected, String description) {
+	@Test(dataProvider = "maxData")
+	public void maxTest(int[] in, int expected, String description) {
+		int actual = SequenceSum.max(in);
+		assertEquals(actual, expected);
+	}
+
+	@DataProvider
+	public Object[][] maxData() {
+		return new Object[][] {
+				{ new int[] { 1 }, 1, "len 1" },
+				{ new int[] { 2, -8, 3, -2, 4, -10 }, 5,
+						"mix of positive and negative integers" },
+				{ new int[] { -3, -10, -5 }, -3,
+						"all negative integers, returned value is first element" },
+				{ new int[] { -3, -10, -5, -1, -11 }, -1,
+						"all negative integers, returned value in the middle" } };
+	}
+
+	@Test(dataProvider = "containsData")
+	public void containsTest(int[] a, int t, boolean expected,
+			String description) {
 		boolean actual = SequenceSum.contains(a, t);
 		assertEquals(actual, expected);
 	}
 
 	@DataProvider
-	public Object[][] data() {
-		return new Object[][] { { new int[] { 23, 5, 4, 7, 2, 11 }, 20, true, "sequence at tail end" },
+	public Object[][] containsData() {
+		return new Object[][] {
+				{ new int[] { 23, 5, 4, 7, 2, 11 }, 20, true,
+						"sequence at tail end" },
 				{ new int[] { 1, 3, 5, 23, 2 }, 8, true, "sequence in middle" },
-				{ new int[] { 1, 3, 5, 23, 2 }, 7, false, "no sequence - sequence broken by a number larger than target" },
+				{ new int[] { 1, 3, 5, 23, 2 }, 7, false,
+						"no sequence - sequence broken by a number larger than target" },
 				{ new int[] { 1, 3, 5, 23, 2 }, 9, true, "sequence at head" },
-				{ new int[] { 1, 3, 5, 23, 2 }, 23, true, "sequence length is 1" } };
+				{ new int[] { 1, 3, 5, 23, 2 }, 23, true,
+						"sequence length is 1" } };
 	}
 
 }
